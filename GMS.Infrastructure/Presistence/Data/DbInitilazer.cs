74a640d126d6f272ac8a7ce2175cee4aa8cbe063
A7MED <ahmed22334455k@gmail.com>
2026-03-17 04:13:50 +0200

    Add DataSeeding In Plans & Catregories

--- GMS.Infrastructure/Presistence/Data/DbInitilazer.cs
+++ GMS.Infrastructure/Presistence/Data/DbInitilazer.cs
@@ -1,9 +1,12 @@
 ﻿using Domin.Contract;
+using Domin.GymEntities;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Presistence.Data {
@@ -17,7 +20,31 @@ namespace Presistence.Data {
             } catch (Exception) {
                 throw;
             }
-            // Data Seeding
+
+            // Data Seeding --> Categories
+            if (!_dbContext.Categories.Any()) {
+                var categoriesData = LoadDataFromJsonFile<Category>("categories.json");
+                if (categoriesData.Any()) _dbContext.Categories.AddRange(categoriesData);
+                _dbContext.SaveChanges();
+            }
+
+            // Data Seeding --> Plans
+            if (!_dbContext.Plans.Any()) {
+                var plansData = LoadDataFromJsonFile<Plan>("plans.json");
+                if (plansData.Any()) _dbContext.Plans.AddRange(plansData);
+                _dbContext.SaveChanges();
+            }
+        }
+
+
+        private List<T> LoadDataFromJsonFile<T>(string fileName) {
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\Data", fileName);
+            if (!File.Exists(filePath)) throw new FileNotFoundException();
+            string data = File.ReadAllText(filePath);
+            var options = new JsonSerializerOptions() {
+                PropertyNameCaseInsensitive = true,
+            };
+            return JsonSerializer.Deserialize<List<T>>(data, options) ?? new List<T>();
         }
     }
 }
